2d15544be86ef497d283e1c2768ecef2512e80d8
hanan <Hanan2010821>
2017-04-17 18:25:57 +0800

    add product detail

---
+++ src/views/app/product/detail.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <c-title :text="title" :hide="true"></c-title>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+  import cTitle from 'components/title';
+
+  export default {
+    data () {
+      return {
+        title: '产品详情',
+      }
+    },
+    
+    mounted: function () {
+      
+    },
+    methods: {
+      
+      test () {
+        window.nativeOpen('test');
+      },
+
+      apply () {
+        window.nativeOpen('apply');
+      }
+    },
+    components: {cTitle}
+  }
+</script>
