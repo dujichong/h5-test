fa9ab46bec1bc8e5e99d27763d3f6e8d2ea47341
hanan <Hanan2010821>
2017-04-18 17:23:57 +0800

    add new file

---
+++ src/components/nav.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="components-nav">
+    <div v-if="showBack" @click="back" class="back"></div>
+    <div v-for="(item, index) of list" @click="change(item, index)" class="item">{{item}}</div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    props: ['list', 'showBack'],
+    methods: {
+      change (item, index) {
+        this.$emit('change', item, index);
+      },
+      back () {
+        this.$emit('back');
+      }
+    }
+  }
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+$mg: 30rem;
+$btn-color: #666;
+$blue: #66b3ff;
+$h: 87rem;
+.components {
+  padding: $mg;
+  box-sizing: border-box;
+  height: 87rem;
+  div {
+    height: $h;
+    line-height: $h;
+  }
+  .back {
+    background: url("../../");
+  }
+}
+</style>
--- src/views/app/product/detail.vue
+++ src/views/app/product/detail.vue
@@ -1,17 +1,54 @@
 <template>
   <div>
     <c-title :text="title" :hide="true"></c-title>
-    <p>{{ message }}</p>
+    <c-nav :list="navs" :showBack="true" @back="back" @change="change"></c-nav>
   </div>
 </template>
 
 <script>
   import cTitle from 'components/title';
+  import cNav from 'components/nav';
 
   export default {
     data () {
       return {
         title: '产品详情',
+        currentIndex: 0,
+        products: [
+          {
+            name: '产品1',
+            rate: '2.28',
+            months: '12-36',
+            money: '30W',
+            synopsis: '简介',
+            introduce: '介绍',
+            quota: '额度期限',
+            area: '地区',
+            competency: ['资格1','资格2','资格3','资格4',],
+            material: '材料'
+          },
+          {
+            name: '产品2',
+            rate: '1.28',
+            months: '12-36',
+            money: '30W',
+            synopsis: '简介',
+            introduce: '介绍',
+            quota: '额度期限',
+            area: '地区',
+            competency: ['资格1','资格2','资格3','资格4',],
+            material: '材料'
+          }
+        ]
+      }
+    },
+
+    computed: {
+      navs () {
+        return this.products.map(item => item.name);
+      },
+      currentDetail () {
+        return this.products[this.currentIndex];
       }
     },
     
@@ -19,15 +56,37 @@
       
     },
     methods: {
+
+      // tab 却换
+      change (item, index) {
+        this.currentIndex = index;    
+      },
+
+      // 后退
+      back () {
+        window.nativeCloseWebview();
+      },
       
+      // 试试我能贷多少
       test () {
         window.nativeOpen('test');
       },
 
+      // 申请
       apply () {
         window.nativeOpen('apply');
       }
     },
-    components: {cTitle}
+    components: {cTitle, cNav}
   }
 </script>
+
+<style lang="scss" rel="stylesheet/scss">
+$mg: 30rem;
+$btn-color: #666;
+$text-color: #5f788d;
+$bg-color: #f1f1f1;
+$blue: #66b3ff;
+$red: #ff5a00;
+
+</style>
