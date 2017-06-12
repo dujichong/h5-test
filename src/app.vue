4c2cb260eca1d3829795290f1fee768658bf3e6d
hanan <Hanan2010821>
2017-06-12 15:26:18 +0800

    change the function to call native

--- src/app.vue
+++ src/app.vue
@@ -29,6 +29,7 @@
       if (native.getDeviceInfo) {
         const jsonDeviceInfo = native.getDeviceInfo();
         const dataDeviceInfo = JSON.parse(jsonDeviceInfo);
+        alert(jsonDeviceInfo);
         this.$store.commit('pid', dataDeviceInfo.pid);
         this.$store.commit('version', dataDeviceInfo.version);
         this.$store.commit('token', dataDeviceInfo.token);
