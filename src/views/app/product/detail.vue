87253255008d6805f343f461c7c96725209fbbde
hanan <Hanan2010821>
2017-06-14 15:37:34 +0800

    control button show or hide

--- src/views/app/product/detail.vue
+++ src/views/app/product/detail.vue
@@ -153,7 +153,7 @@
 
       // 申请
       apply() {
-        native.open(this.token ? 'jump_to_apply"' : 'jump_to_login');
+        native.open(this.token ? 'jump_to_apply' : 'jump_to_login');
       }
     },
     components: {cTitle, cNav}
